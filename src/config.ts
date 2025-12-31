f4b33cadf91dddc24762ac6612de39b54eae8654
JustDreemurr <azzy123@foxmail.com>
2025-12-31 17:16:33 +0800

    Revert "test"
    This reverts commit 00e55fdf72cd7a1167743159f79192636a691048.

--- src/config.ts
+++ src/config.ts
@@ -196,9 +196,9 @@ export const siteConfig: SiteConfig = {
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "萝莉体 第二版",
+			fontFamily: "OPPOSans4.0圆体",
 			fontWeight: "500",
-			localFonts: ["萝莉体 第二版.ttf"],
+			localFonts: ["OPPOSans4.0圆体.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
